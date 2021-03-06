5f8ee5237256940796f4231c56155522de56cb0e
dele926 <adelesunnywang@gmail.com>
2021-03-06 20:16:27 +0000

    SQL Server server.js trial

--- server.js
+++ server.js
@@ -28,6 +28,55 @@ app.get("/dreams", (request, response) => {
   response.json(dreams);
 });
 
+//app.put("/customerlogin", ())
+
+var Connection = require('tedious').Connection
+var Request = require('tedious').Request
+
+var config = {
+  server: 'ec2-54-90-13-87.compute-1.amazonaws.com',
+  authentication: {
+    type: 'default',
+    options: {
+      userName: 'xcqtoyzzdrwejn', // update me
+      password: 'c58307a93c448b0f3a1d1a373bbc2e28f867c446cda614e8d43303e474784813' // update me
+    }
+  }
+}
+
+var connection = new Connection(config)
+
+connection.on('connect', function (err) {
+  if (err) {
+    console.log(err)
+  } else {
+    executeStatement()
+  }
+})
+
+function executeStatement () {
+  request = new Request("SELECT * from inventory;", function (err, rowCount) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(rowCount + ' rows')
+    }
+    connection.close()
+  })
+
+  request.on('row', function (columns) {
+    columns.forEach(function (column) {
+      if (column.value === null) {
+        console.log('NULL')
+      } else {
+        console.log(column.value)
+      }
+    })
+  })
+  connection.execSql(request)
+}
+
+
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
